114e97ee3524afd37d59a4aaacb6b71fe578ec9e
Paul Shen <pxshen@alumni.stanford.edu>
2020-08-16 17:14:03 -0400

    some message

---
+++ push.bat
@@ -0,0 +1,6 @@
+cd C:\Users\Paul\Desktop\mednlp\paulxshen.github.io
+git add -A
+git commit -m "some message"
+git push https://paulxshen:19GOLDENCURRYhuzi89@github.com/paulxshen/paulxshen.github.io
+::git clone https://pshen1230:19GOLDENCURRYhuzi89@github.com/pshen1230/mednlp
+::pause
